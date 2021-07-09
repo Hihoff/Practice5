c8038a80621a83dcfd20653110e3634c36b28e4f
Hihoff <52756084+Hihoff@users.noreply.github.com>
2021-07-09 15:15:50 +0300

    Add files via upload

---
+++ Практика5/Практика5/LoginForm1.cs
@@ -0,0 +1,86 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика5
+{
+    public partial class LoginForm1 : Form
+    {
+        public LoginForm1()
+        {
+            InitializeComponent();
+
+            this.password_autorization.AutoSize = false;
+            this.password_autorization.Size = new Size(this.password_autorization.Width, 44);
+            
+            this.login_autorization.AutoSize = false;
+            this.login_autorization.Size = new Size(this.login_autorization.Width, 44);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        Point LastPoint;
+        private void MainPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - LastPoint.X;
+                this.Top += e.Y - LastPoint.X;
+            }
+        }
+        private void MainPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            LastPoint = new Point(e.X, e.Y);
+        }
+
+        private void label_Top_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - LastPoint.X;
+                this.Top += e.Y - LastPoint.X;
+            }
+        }
+        private void label_Top_MouseDown(object sender, MouseEventArgs e)
+        {
+            LastPoint = new Point(e.X, e.Y);
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string loginUser = login_autorization.Text;
+            string passUser = password_autorization.Text;
+
+            DataBase db = new DataBase();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE 'login' = @uL AND 'password' = @uP", db.getConnection());
+            command.Parameters.Add("@uL", MySqlDbType.VarChar).Value = loginUser;
+            command.Parameters.Add("@uP", MySqlDbType.VarChar).Value = passUser;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+              MessageBox.Show("Авторизация прошла успешно");
+            else
+              MessageBox.Show("Авторизация не выполнена"); 
+            
+        }
+    }
+}
+
